e4858735c22351a6f4ad3fb2e80b442f8a4bfc8e
deezy13 <deezy13@gmail.com>
2019-10-26 18:28:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,6 +41,12 @@ def self.alphabetical()
     #returns all the songs instances in ascending (a-z) alphabetical order.
     @@all.sort_by{|x| x.name}
   end
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
   
   def self.all
     @@all
