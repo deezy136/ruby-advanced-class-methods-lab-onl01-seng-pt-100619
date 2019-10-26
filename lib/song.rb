bc31409dd463df9ccd56840709996f68aa609a58
deezy13 <deezy13@gmail.com>
2019-10-26 18:28:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,16 @@ def self.alphabetical()
     song
   end
   
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+  
+  
   def self.all
     @@all
   end
